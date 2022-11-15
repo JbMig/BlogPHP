b6fd79d8bc5f693dfffb168960dd38b38021a88b
ValentineLefebvre <v_lefebvre2@hetic.eu>
2022-11-15 16:00:58 +0100

    noms changés pour l'hydratation

--- database/dump.sql
+++ database/dump.sql
@@ -3,31 +3,31 @@ CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 use db_blog_novembre;
 
-CREATE TABLE IF NOT EXISTS `users` (
-	`user_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `user` (
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(255) NOT NULL UNIQUE,
 	`password` VARCHAR(256) NOT NULL,
-	`pseudo` VARCHAR(255),
+	`nickname` VARCHAR(255),
 	`admin` ENUM('true','false') DEFAULT 'false',
-	PRIMARY KEY (`user_id`)
+	PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `articles` (
-	`article_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `post` (
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`content` TEXT,
 	`date` DATETIME DEFAULT CURRENT_TIMESTAMP,
 	`user_id` INT NOT NULL,
-	PRIMARY KEY (`article_id`),
+	PRIMARY KEY (`id`),
 	CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB;
 
-CREATE TABLE IF NOT EXISTS `comments` (
-	`comment_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `comment` (
+	`id` INT NOT NULL AUTO_INCREMENT,
 	`content` TEXT,
 	`date` DATETIME DEFAULT CURRENT_TIMESTAMP,
 	`user_id` INT NOT NULL,
-	`previous_comment_id` INT NOT NULL,
-	PRIMARY KEY (`comment_id`),
+	`previous_post_id` INT NOT NULL,
+	PRIMARY KEY (`id`),
 	CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `users`(`user_id`) ON DELETE CASCADE
-	CONSTRAINT FOREIGN KEY (`previous_comment_id`) REFERENCES `comments`(`comment_id`) ON DELETE CASCADE
+	CONSTRAINT FOREIGN KEY (`previous_post_id`) REFERENCES `post`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB;
