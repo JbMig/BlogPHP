76d4e809970237945f68c1a39cabfd8875e11dfb
JbMig <jbmigone@gmail.com>
2022-11-17 15:28:17 +0100

    vendor

---
+++ app/src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'root/html',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '1a610f607f3a4d41ac700792f121fba380df0071',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'root/html' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '1a610f607f3a4d41ac700792f121fba380df0071',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
